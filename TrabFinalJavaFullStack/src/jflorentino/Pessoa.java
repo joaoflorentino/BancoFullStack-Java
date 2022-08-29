1c95ffed8fcca5a9b5ac649c9100d86332f3c96c
Joao Florentino <joao.florentino.senai@outlook.com>
2022-08-29 10:26:05 -0300

    atualizacoes do java no projeto final

---
+++ TrabFinalJavaFullStack/src/jflorentino/Pessoa.java
@@ -0,0 +1,19 @@
+package jflorentino;
+
+public class Pessoa {
+	private static String nome;
+	
+	public Pessoa(String nome) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	
+	
+}
